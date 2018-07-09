c5ca6906e6b708fd67a32ccf940a354de4fdec13
unknown <hermano.mark.mh@gmail.com>
2018-07-09 14:56:45 +0800

    react part 1 done

--- React/3.3. Components Interacting - this.state/main.js
+++ React/3.3. Components Interacting - this.state/main.js
@@ -196,7 +196,7 @@ this.makeSomeFog = this.makeSomeFog.bind(this);
 
 // That is why we must bind this.toggleMood to this on line 8.
 
-// For an in-depth explanation of this kind of binding trickery, begin with the React docs. For the less curious, just know that in React, whenever you define an event handler that uses this, you need to add this.methodName = this.methodName.bind(this) to your constructor function.
+// For an in-depth explanation of this kind of binding trickery, begin with the React docs (https://reactjs.org/docs/handling-events.html). For the less curious, just know that in React, whenever you define an event handler that uses this, you need to add this.methodName = this.methodName.bind(this) to your constructor function.
 
 // Look at the statement on line 12. What does that do?
 
@@ -273,3 +273,72 @@ changeColor() {
 ReactDOM.render(<Toggle />, document.getElementById('app'));
 
 // ------------------------------------------------------------
+// this.setState Automatically Calls render
+// There's something odd about all of this.
+
+// Look again at Toggle.js.
+
+// When a user clicks on the <button></button>, the .changeColor() method is called. Take a look at .changeColor()'s definition.
+
+// .changeColor() calls this.setState(), which updates this.state.color. However, even if this.state.color is updated from green to yellow, that alone shouldn't be enough to make the screen's color change!
+
+// The screen's color doesn't change until Toggle renders.
+
+// Inside of the render function, it's this line:
+
+<div style={{background:this.state.color}}>
+</div>
+
+// that changes a virtual DOM object's color to the new this.state.color, eventually causing a change in the screen.
+
+// If you call .changeColor(), shouldn't you then also have to call .render() again? .changeColor() only makes it so that, the next time that you render, the color will be different. Why can you see the new background right away, if you haven't re-rendered the component?
+
+// Here's why: Any time that you call this.setState(), this.setState() AUTOMATICALLY calls .render() as soon as the state has changed.
+
+// Think of this.setState() as actually being two things: this.setState(), immediately followed by .render().
+
+// That is why you can't call this.setState() from inside of the .render() method! this.setState() automatically calls .render(). If .render() calls this.setState(), then an infinite loop is created.
+
+// Sample
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const green = '#39D1B4';
+const yellow = '#FFD712';
+
+class Toggle extends React.Component {
+  constructor(props) {
+  super(props);
+  this.state = { color: green };
+  this.changeColor = this.changeColor.bind(this);
+}
+
+changeColor() {
+  const newColor = this.state.color == green ? yellow : green; 
+  this.setState({color: newColor });
+}
+  render() {
+    return (
+      <div style={{background: this.state.color}}>
+        <h1>
+          Change my color
+        </h1>
+        <button onClick={this.changeColor}>
+          Change color
+          </button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Toggle />, document.getElementById('app'));
+
+// ------------------------------------------------------------
+// Components Interacting Recap
+// In this unit, you learned how to use import and export to access one file from another. You learned about props and state, and the countless variations on how they work.
+
+// Before this unit, you learned about JSX, components, and how they can work together.
+
+// A React app is basically just a lot of components, setting state and passing props to one another. Now that you have a real understanding of how to use props and state, you have by far the most important tools that you need!
+
+// In future lessons, the focus will shift slightly outward. In addition to learning more new skills, you'll also learn your first programming patterns. These large-scale strategies will help you combine what you've learned and really start building.
